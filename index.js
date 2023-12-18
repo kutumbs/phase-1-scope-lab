9e26029a2a3ee416d0f26d52fc536375af8c7f2a
jonathan <jonathan.123kutumbakana@gmail.com>
2023-12-18 15:47:54 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase()
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer = `not bob`;
+}function overwriteBestCustomer(){
+    bestCustomer = `maybe bob`;
+}
+const leastFavoriteCustomer = `robins`;
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = `collins`;
+    return leastFavoriteCustomer
+}
+
+
